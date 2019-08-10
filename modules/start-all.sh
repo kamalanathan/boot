cc64d19e2b2c5ce42369b2db9dcacfa8b95bdea0
kamal <kamalanathan.v@gmail.com>
2019-08-11 01:10:19 +0530

    my changes

--- modules/start-all.sh
+++ modules/start-all.sh
@@ -21,13 +21,13 @@ scp ./DemandSupplyFE/target/*.jar kamal@slave04:/home/kamal/Documents/workspace/
 java -jar ./eurekaserver/target/eurekaserver-0.0.1-SNAPSHOT.jar &
 sleep 5s
 java -jar ./cloud-config/target/cloud-config-0.0.1-SNAPSHOT.jar &
-sleep 5s
+sleep 10s
 java -jar ./matcher/target/matcher-0.0.1-SNAPSHOT.jar &
 
 ssh kamal@slave02 'java -jar /home/kamal/Documents/workspace/DemandSupplyFE/demandsupplyfe-0.0.1-SNAPSHOT.jar &'
 ssh kamal@slave04 'java -jar /home/kamal/Documents/workspace/DemandSupplyFE/demandsupplyfe-0.0.1-SNAPSHOT.jar &'
 
-ssh kamal@slave02 'java -jar /home/kamal/Documents/workspace/profiles/target/profiles-0.0.1-SNAPSHOT.jar &'
-ssh kamal@slave04 'java -jar /home/kamal/Documents/workspace/profiles/target/profiles-0.0.1-SNAPSHOT.jar &'
+ssh kamal@slave02 'java -jar /home/kamal/Documents/workspace/profiles/profiles-0.0.1-SNAPSHOT.jar &'
+ssh kamal@slave04 'java -jar /home/kamal/Documents/workspace/profiles/profiles-0.0.1-SNAPSHOT.jar &'
 
 java -jar ./zuulfilter/target/zuulfilter-0.0.1-SNAPSHOT.jar &
