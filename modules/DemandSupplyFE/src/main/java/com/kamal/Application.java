d4e9552ff26b885b3941d224625d9882a7cf2bd3
kamal <kamalanathan.v@gmail.com>
2019-08-15 21:04:24 +0530

    test

--- modules/DemandSupplyFE/src/main/java/com/kamal/Application.java
+++ modules/DemandSupplyFE/src/main/java/com/kamal/Application.java
@@ -1,5 +1,7 @@
 package com.kamal;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +9,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import com.kamal.oauth.UserContextFilter;
 
 import brave.sampler.Sampler;
 
@@ -16,11 +20,11 @@ import brave.sampler.Sampler;
 @EnableFeignClients("com.kamal")
 @ComponentScan(basePackages = "com.kamal")
 public class Application {
-	// @Bean
-	// public Filter userContextFilter() {
-	// UserContextFilter userContextFilter = new UserContextFilter();
-	// return userContextFilter;
-	// }
+	@Bean
+	public Filter userContextFilter() {
+		UserContextFilter userContextFilter = new UserContextFilter();
+		return userContextFilter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
